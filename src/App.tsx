7b7fd273411375d52feb25a70f4ad24d74382dfc
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-15 15:49:19 +0300

    new edits

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ const App = () => (
   <div className="App">
     <WolfGallery
       photos={[
-        { id: 1, src: '/photos/1.jpg', preview: '/photos/preview/1.jpg', description: 'Norway 1' },
-        { id: 2, src: '/photos/2.jpg', preview: '/photos/preview/2.jpg', description: 'Tver Region — Staritsa' },
-        { id: 3, src: '/photos/3.jpg', preview: '/photos/preview/3.jpg', description: 'Norway 3' },
-        { id: 4, src: '/photos/4.jpg', preview: '/photos/preview/4.jpg', description: 'Norway 4' },
-        { id: 5, src: '/photos/5.jpg', preview: '/photos/preview/5.jpg', description: 'Norway 5' },
-        { id: 6, src: '/photos/6.jpg', preview: '/photos/preview/6.jpg', description: 'Norway 6' },
-        { id: 7, src: '/photos/7.jpg', preview: '/photos/preview/7.jpg', description: 'Norway 7' },
+        { id: 1, src: '../public/photos/1.jpg', preview: '../public/photos/preview/1.jpg', description: 'Photo 1' },
+        { id: 2, src: '../public/photos/2.jpg', preview: '../public/photos/preview/2.jpg', description: 'Photo 2' },
+        { id: 3, src: '../public/photos/3.jpg', preview: '../public/photos/preview/3.jpg', description: 'Photo 3' },
+        { id: 4, src: '../public/photos/4.jpg', preview: '../public/photos/preview/4.jpg', description: 'Photo 4' },
+        { id: 5, src: '../public/photos/5.jpg', preview: '../public/photos/preview/5.jpg', description: 'Photo 5' },
+        { id: 6, src: '../public/photos/6.jpg', preview: '../public/photos/preview/6.jpg', description: 'Photo 6' },
+        { id: 7, src: '../public/photos/7.jpg', preview: '../public/photos/preview/7.jpg', description: 'Photo 7' },
       ]}
     />
   </div>
